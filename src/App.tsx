58242e198780892a2fc3aff93b9dba1741887873
Marlee Garcia <marlee.garcia121@gmail.com>
2022-07-21 21:58:39 -0400

    Made functional buttons but I am so tired

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import AdDesigner from './components/AdDesigner';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className='content-container'>
+        <AdDesigner />
+      </div>
     </div>
   );
 }
--- src/components/AdDesigner.tsx
+++ src/components/AdDesigner.tsx
@@ -1,1 +1,51 @@
+import { isDisabled } from "@testing-library/user-event/dist/utils";
+import { useState } from "react";
 import "./AdDesigner.css";
+
+
+
+function AdDesigner() {
+    const [flavor, setFlavor] = useState("Strawberry");
+    const [mode, setMode] = useState("-light");
+
+    const handleChocolateButton = () => {
+        setFlavor("Chocolate");
+    }
+
+    const handleVanillaButton = () => {
+        setFlavor("Vanilla");
+    }
+
+    const handleStrawberryButton = () => {
+        setFlavor("Strawberry");
+    }
+
+    const handleLightButton = () => {
+        setMode("-light");
+    }
+    
+    const handleDarkButton = () => {
+        setMode("-dark");
+    }
+    return (<div style={{marginLeft: '8px'}}>
+            <h2 className="caveat-font">Ad Designer</h2>
+            <div className= {"ad" + mode} style={{display: 'flex', flexDirection: 'column', textAlign: 'center', border: '2px solid black', width: "110px"}}>
+                <p>Vote For</p><h3>{flavor}</h3>
+            </div>
+            <div>
+                <p>What to Support</p>
+                <button onClick={handleChocolateButton} disabled={flavor === "Chocolate"}>Chocolate</button>
+                <button onClick={handleVanillaButton} disabled={flavor === "Vanilla"}>Vanilla</button>
+                <button onClick={handleStrawberryButton} disabled={flavor === "Strawberry"}>Strawberry</button>
+            </div>
+            <div>
+                <p>Color Theme</p>
+                <button onClick={handleLightButton} disabled={mode === "-light"}>Light</button>
+                <button onClick={handleDarkButton} disabled={mode === "-dark"}>Dark</button>
+            </div>
+        
+        </div>
+    )
+}
+
+export default AdDesigner;
